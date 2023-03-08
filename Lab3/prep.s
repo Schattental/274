1893beb96fffc6417aaf86f65b9fbc14158f9ef3
Benedikt Gregor <19bg15@queensu.ca>
2023-03-08 05:30:15 +0100

    updated prep for lab3

--- Lab3/prep.s
+++ Lab3/prep.s
@@ -21,11 +21,13 @@ _start:
 	movia 	r2, TEXT
 	call	PrintString
 	
+	movi	r2, 0x4A
+	call 	PrintHexByte
+	
 	break	
 	
 PrintChar:
-	subi	sp, sp, 12
-	stw		ra, 8(sp)
+	subi	sp, sp, 8
 	stw		r3, 4(sp)
 	stw		r4, 0(sp)
 	movia	r3, JTAG_UART_BASE
@@ -35,14 +37,14 @@ pc_loop:
 	beq		r4, r0, pc_loop
 	stwio	r2, DATA_OFFSET(r3)
 	
-	ldw		ra, 8(sp)
 	ldw		r3, 4(sp)
 	ldw		r4, 0(sp)
-	addi	sp, sp, 12
+	addi	sp, sp, 8
 	ret
 	
 PrintString:
-	subi	sp, sp, 8
+	subi	sp, sp, 12
+	stw		ra, 8(sp)
 	stw		r2, 4(sp)
 	stw		r3, 0(sp)
 	
@@ -56,9 +58,58 @@ ps_loop:
 	br		ps_loop
 	
 ps_loop_end:
+	ldw 	ra, 8(sp)
 	ldw		r2, 4(sp)
 	ldw		r3, 0(sp)
-	addi	sp, sp, 8
+	addi	sp, sp, 12
+	
+	ret
+	
+PrintHexDigit:
+	subi 	sp, sp, 12
+	stw		r2, 8(sp)
+	stw		r3, 4(sp)
+	stw		ra, 0(sp)
+	
+phd_if:
+	movi 	r3, 10
+	bge		r2, r3, phd_else
+	
+phd_then:
+	addi 	r2, r2, '0'
+	br 		phd_end_if
+	
+phd_else:
+	subi 	r2, r2, 10
+	addi 	r2, r2, 'A'
+	
+phd_end_if:
+	call 	PrintChar
+	
+	ldw		r2, 8(sp)
+	ldw		r3, 4(sp)
+	ldw		ra, 0(sp)
+	addi 	sp, sp, 12
+	
+	ret
+	
+PrintHexByte:
+	subi 	sp, sp, 12
+	stw		r2, 8(sp)
+	stw		r3, 4(sp)
+	stw		ra, 0(sp)
+	
+	mov 	r3, r2
+	
+	srli	r2, r3, 4
+	call 	PrintHexDigit
+	andi 	r2, r3, 0xF
+	call 	PrintHexDigit
+	
+	ldw		r2, 8(sp)
+	ldw		r3, 4(sp)
+	ldw		ra, 0(sp)
+	addi 	sp, sp, 12
 	
 	ret
 	
